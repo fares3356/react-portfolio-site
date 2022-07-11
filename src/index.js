4f694b8116c13a9fe24e9edcc839fa917f4c90ef
Fares Akhtar <faresakhtar@MacBook-Pro-2.local>
2022-07-10 20:57:18 -0400

    adding all files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// Here we import bootstrap for access to some additional styling
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
